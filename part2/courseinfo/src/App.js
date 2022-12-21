0f2b1acfa565d99ca80553e411091e6cadbec533
elliotjeisenberg <elliotjeisenberg@gmail.com>
2022-12-20 21:59:18 -0600

    beginning of 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,18 +1,31 @@
-const App = (props) => {
-  const { notes } = props
+import Course from './components/Course'
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-        <li key={note.id}>
-          {note.content}
-        </li>
-        )}
-      </ul>
-    </div>
-  )
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
