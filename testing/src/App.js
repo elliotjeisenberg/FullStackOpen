eb04db90218c41e1dd57b66524c078b27efb0458
elliotjeisenberg <elliotjeisenberg@gmail.com>
2022-12-25 15:56:42 -0600

    2d practice

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,30 +1,86 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-    
-  const hook = () => {
-    console.log('effect')
+  useEffect(() => {
+    noteService
+    .getAll()
+    .then(response => {
+      setNotes(response)
+    })
+      
+  }, [])
 
-    const eventHandler = (response) => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    }
+  const toggleImportance = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
 
-    const promise = axios.get('http://localhost:3001/notes')
-    promise.then(eventHandler)
+    noteService
+    .update(id, changedNote)
+    .then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response))
+    })
 
+    axios.put(url, changedNote)
+    .then(res => {
+      setNotes(notes.map(n => n.id !== id ? n : res))
+    })
   }
 
-  useEffect(hook, [])
-  
-  console.log('render', notes.length, 'notes')
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
 
-  // ...
+    noteService
+    .create(noteObject)
+    .then(response => {
+      setNotes(notes.concat(response))
+      setNewNote('')
+    })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)}/>
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
--- testing/src/index.js
+++ testing/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import App from './App';
 import axios from 'axios'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App notes={notes} />
+  )
+})
