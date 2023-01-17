b559a8154c4de8558c5f012acff57a1f0d088fd2
elliotjeisenberg <elliotjeisenberg@gmail.com>
2023-01-16 20:13:49 -0600

    updating part2 testing

--- part2/testing/index.js
+++ part2/testing/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.json)
+app.use(express.json())
 
 let notes = [
   {
@@ -39,18 +39,41 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
-  console.log('new notes', notes)
   response.status(204).end()
 })
 
-// app.post('/api/notes', (request, response) => {
-//   const note = request.body
-//   console.log(note)
-//   response.json(note)
-// })
+const generateId = () => {
+  const maxId = notes.length > 0 
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error:'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+
+})
 
 
 const PORT = 3000
