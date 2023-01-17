477fac4134ed89b38708eba89703cc267bce3ef0
elliotjeisenberg <elliotjeisenberg@gmail.com>
2023-01-17 07:53:30 -0600

    refactored blogsRouter

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./build/blog')
+const blogsController = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -12,23 +13,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsController)
 
 const PORT = 3003
 app.listen(PORT, () => {
