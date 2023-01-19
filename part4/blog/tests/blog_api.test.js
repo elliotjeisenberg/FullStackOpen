aed125c171ac3df4abf3c659cea116ff4b3887bc
elliotjeisenberg <elliotjeisenberg@gmail.com>
2023-01-19 08:11:48 -0600

    starting to do awaits

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../build/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    console.log('Cleared test database')
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save)
+    await Promise.all(promiseArray)
+
+    await api.get('/api/blogs')
+})
 
 test('blogs are returned as json', async () => {
     await api
