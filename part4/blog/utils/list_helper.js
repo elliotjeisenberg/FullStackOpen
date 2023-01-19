27e40b33bd10a3514e7ebd39ccb4274a04fc5b7f
elliotjeisenberg <elliotjeisenberg@gmail.com>
2023-01-18 23:54:12 -0600

    Completed 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,24 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+     return blogs.reduce((accumulator, blog) => {
+        return accumulator + blog.likes
+     },0)
+};
+
+const favoriteBlog = (blogs) => {
+    const favBlog = blogs[Object.keys(blogs).sort((a,b) => blogs[b].likes - blogs[a].likes)[0]]
+    
+    return {
+        title: favBlog['title'],
+        author: favBlog['author'],
+        likes: favBlog['likes']
+    }
+};
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
