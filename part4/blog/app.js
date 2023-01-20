fa749a8496c1699553d9db2cc79e9624d80cce14
elliotjeisenberg <elliotjeisenberg@gmail.com>
2023-01-19 18:13:08 -0600

    finished through 4.9

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,10 +1,12 @@
+const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./build/blog')
 const blogsController = require('./controllers/blogs')
-const config = require('./config/config')
+const middleware = require('./utils/middleware')
 
 
 mongoose.connect(config.MONGODB_URI)
@@ -13,5 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsController)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -12,20 +12,38 @@ beforeEach(async () => {
 
     const blogObjects = helper.initialBlogs
         .map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save)
+    const promiseArray = blogObjects.map(blog => blog.save())
     await Promise.all(promiseArray)
-
-    await api.get('/api/blogs')
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('blog list tests', () => {
+    test('blogs are returned as json', async () => {
+        const result = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type',/application\/json/)
+    
+        expect(result.body.length).toBe(helper.initialBlogs.length)
+    })
 
+    test('unique identifier is "id"', async () => {
+        const result = await api
+            .get('/api/blogs')
+        
+        const uniqueIDList = {}
 
+        //check that each blog has an id and add that to the id hash
+        const blogs = result.body
+        blogs.forEach((blog) => {
+            expect(blog.id).toBeDefined()
+            uniqueIDList[blog.id] = null
+        })
+
+        //check that no id was added to the unique list more than once
+        const totalUniqueIDs = Object.keys(uniqueIDList).length
+        expect(totalUniqueIDs).toBe(helper.initialBlogs.length)
+    })
+})
 
 afterAll(() => {
     mongoose.connection.close()
